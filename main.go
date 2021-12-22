2452e5c52f9571b7ce32cffa3e5a6b6d4d081196
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-12-21 23:17:46 -0800

    creating accessing an array

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+func main() {
+	// arrays
+
+	var arr [3]int
+	arr[0] = 1
+	arr[1] = 2
+	arr[2] = 3
+	//output [1 2 3]
+	fmt.Println(arr)
+
+	//ouput [2]
+	fmt.Println(arr[1])
+
+	// implicit initiationzation format
+	numArray := [3]int{ 5, 6, 7}
+	//output [5 6 7]
+	fmt.Println(numArray)
+
+
+
+	// slices
+
+
+
+	// maps
+
+
+
+	// structs
+
+
+}
+
